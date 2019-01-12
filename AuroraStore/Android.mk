54ffd7344e1c56e315a66861cbbaf3b22c2d74ae
60Eight <23185944+60eight@users.noreply.github.com>
2019-01-13 04:38:34 +1100

    Add AuroraStore and LineageOS Recorder pre-built binaries

---
+++ AuroraStore/Android.mk
@@ -0,0 +1,20 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := privapp-permissions-com.dragons.aurora.xml
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_PATH := $(TARGET_OUT_ETC)/permissions
+LOCAL_SRC_FILES := $(LOCAL_MODULE)
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE := AuroraStore
+LOCAL_SRC_FILES := com.dragons.aurora_210020005.apk
+LOCAL_MODULE_CLASS := APPS
+LOCAL_PRIVILEGED_MODULE := true
+LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
+LOCAL_CERTIFICATE := PRESIGNED
+LOCAL_REQUIRED_MODULES := privapp-permissions-com.dragons.aurora.xml
+include $(BUILD_PREBUILT)
